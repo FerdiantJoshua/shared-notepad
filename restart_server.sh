600f4d422926146ac181a101b9cd2df102c59cac
FerdiantJoshua <ferdiant.98@gmail.com>
2025-02-23 22:43:56 +0700

    Improve stop & restart script

--- restart_server.sh
+++ restart_server.sh
@@ -10,7 +10,11 @@ source venv/bin/activate
 NOHUP_PID=`ps -ef | grep [s]hared-notepad/venv | head -n 1 | awk -F ' ' '{print $2}'`
 if [[ ! -z $NOHUP_PID ]]
 then
+  decho "Killing PID=$NOHUP_PID.."
   kill $NOHUP_PID
+  while kill -0 $NOHUP_PID 2> /dev/null; do
+    sleep 1
+  done
   decho "PID=$NOHUP_PID killed.."
 fi
 
--- stop_server.sh
+++ stop_server.sh
@@ -8,11 +8,12 @@ decho() {
 NOHUP_PID=`ps -ef | grep [s]hared-notepad/venv | head -n 1 | awk -F ' ' '{print $2}'`
 if [[ ! -z $NOHUP_PID ]]
 then
+  decho "Killing PID=$NOHUP_PID.."
   kill $NOHUP_PID
+  while kill -0 $NOHUP_PID 2> /dev/null; do
+    sleep 1
+  done
   decho "PID=$NOHUP_PID killed.."
-else
-  decho "Service is not running"
-  exit 0
 fi
 
 decho "Stopped service succesfully"
